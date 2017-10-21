b942a11f007d822a6be0667bdff638fdd87b75ad
David Basalla <davidbasalla@gmail.com>
2017-10-21 19:02:33 +0100

    Set up tiles

--- js/scene.js
+++ js/scene.js
@@ -11,12 +11,12 @@ var createScene = function () {
 
 
   var camera = new BABYLON.FreeCamera("camera1", 
-                                      new BABYLON.Vector3(10, 6, -10),
+                                      new BABYLON.Vector3(6, 4, -6),
                                       scene);
   camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
 
-  camera.orthoTop = 6;
-  camera.orthoBottom = -6;
+  camera.orthoTop = 4;
+  camera.orthoBottom = -4;
   camera.orthoLeft = -10;
   camera.orthoRight = 10;
 
@@ -29,29 +29,52 @@ var createScene = function () {
   // This attaches the camera to the canvas
   camera.attachControl(canvas, false);
   
-  // This creates a light - aimed 0,1,0 - to the sky.
-  var light = new BABYLON.HemisphericLight("light1", 
-      new BABYLON.Vector3(0, 1, 0), scene);
-  
-  // Dim the light a small amount
-  light.intensity = .75;
-  
+  // SETUP LIGHTS
+  var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+  light.intensity = .5;
+
+  var dir_light = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(1, -1, 1), scene);
+
   // Our built-in 'sphere' shape. Params: name, subdivisions, size, scene
   // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
   var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1}, scene);
+  box.position.x = -1;
 
-  // Move the sphere upward 1/2 its height
-  // box.position.y = 1;
-  
-  // Our built-in 'ground' shape.  Params: name, width, depth, subdivs, scene
+  // SETUP GROUND
   var ground = BABYLON.Mesh.CreateGround("ground1", 35, 35, 2, scene);
-  
+  ground.position.y = -.01
+
+  // SETUP GROUND MATERIAL
   var material = new BABYLON.StandardMaterial("texture1", scene);
   material.specularColor = new BABYLON.Color3(0, 0, 0);
   material.diffuseTexture = new BABYLON.Texture("assets/images/mars_square_4k_v01.jpg", scene);
   // material.bumpTexture = new BABYLON.Texture("assets/images/mars_square_4k_BMP_v02.jpg", scene);
-
   ground.material = material;
+
+
+  // SETUP TILES
+  var tiles = []
+  for(var x = -10; x < 10; x++) {
+    for(var z = -10; z < 10; z++) {
+      console.log("ADD PLANE")
+
+      var tile = BABYLON.MeshBuilder.CreatePlane("tile", {width: 1, height: 1, sideOrientation: 0}, scene);
+      tile.rotation.x = Math.PI/2;
+      tile.position.x = x;
+      tile.position.z = z;
+
+      tiles.push(tile)
+    }
+  }
+
+  // SETUP TILE MATERIAL
+  var wireframe_material = new BABYLON.StandardMaterial("wireframe_texture", scene);
+  wireframe_material.wireframe = true;
+
+  for(i = 0; i < tiles.length; i++){
+    var tile = tiles[i];
+    tile.material = wireframe_material;
+  }
 
 
   // Leave this function
