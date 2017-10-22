11fec3be0ec58f6ee7ae81f6f5a405c3102e3307
David Basalla <davidbasalla@gmail.com>
2017-10-22 13:42:00 +0100

    Import habitat building
    Annoying that I have to revert to older version of Library. Something isn't working correctly with the latest Babylon library. Will need to investigate what exactly...

--- js/scene.js
+++ js/scene.js
@@ -33,13 +33,11 @@ var createScene = function () {
 
   var dir_light = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(1, -1, 1), scene);
 
-  // Our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-  // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-  var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1}, scene);
-  box.position.x = -1;
+  // var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1}, scene);
+  // box.position.x = -1;
 
   // SETUP GROUND
-  var ground = BABYLON.Mesh.CreateGround("ground1", 35, 35, 2, scene);
+  var ground = BABYLON.Mesh.CreateGround("ground1", 30, 30, 2, scene);
   ground.position.y = -.01
 
   // SETUP GROUND MATERIAL
@@ -52,44 +50,95 @@ var createScene = function () {
 
   // SETUP TILES
   var tiles = []
+
+  var wireframe_material = new BABYLON.StandardMaterial("wireframe_texture", scene);
+  wireframe_material.wireframe = true;
+  wireframe_material.alpha = 0.5;
+
   for(var x = -10; x < 10; x++) {
     for(var z = -10; z < 10; z++) {
       var tile = BABYLON.MeshBuilder.CreatePlane("tile", {width: 1, height: 1, sideOrientation: 0}, scene);
       tile.rotation.x = Math.PI/2;
       tile.position.x = x;
       tile.position.z = z;
+      tile.material = wireframe_material;
 
       tiles.push(tile)
     }
   }
 
-  // SETUP TILE MATERIAL
-  var wireframe_material = new BABYLON.StandardMaterial("wireframe_texture", scene);
-  wireframe_material.wireframe = true;
-
-  for(i = 0; i < tiles.length; i++){
-    var tile = tiles[i];
-    tile.material = wireframe_material;
-  }
+  var selected_tile_state = null;
 
   // GUI
-  var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+  // var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-  var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Build Habitat");
-  button1.width = "220px"
-  button1.height = "100px";
-  button1.color = "white";
-  button1.cornerRadius = 10;
-  button1.background = "green";
-  button1.onPointerUpObservable.add(function() {
-      alert("you did it!");
+  // var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Build Habitat");
+  // button1.width = "220px"
+  // button1.height = "100px";
+  // button1.color = "white";
+  // button1.cornerRadius = 10;
+  // button1.background = "green";
+  // button1.onPointerUpObservable.add(function() {
+  //   console.log("BUILD HABITAT");
+
+  //   if (selected_tile_state == null) {
+  //     console.log("NOTHING SELECTED");
+  //   }
+  
+  //   // Create new model at tile position
+  //   var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1}, scene);
+  //   box.position =  selected_tile_state.position;
+
+  // });
+  // advancedTexture.addControl(button1);
+  // button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  // button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  // button1.paddingLeft = 20;
+  // button1.paddingBottom = 20;
+
+
+  //When click event is raised
+  window.addEventListener("click", function (evt) {
+    var pickResult = scene.pick(evt.clientX, evt.clientY);
+    var selected_tile = pickResult.pickedMesh;
+
+    // Return early in case of mesh
+    if (selected_tile == ground) {
+      return;
+    }
+
+    var is_selected_material = new BABYLON.StandardMaterial("texture_xxx", scene);
+    is_selected_material.diffuseColor = new BABYLON.Color3(0, 1, 0);
+    is_selected_material.alpha = 0.35;
+
+    selected_tile.material = is_selected_material
+    selected_tile_state = selected_tile
   });
-  advancedTexture.addControl(button1);    
-  button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
-  button1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  button1.paddingLeft = 20
-  button1.paddingBottom = 20
 
+
+
+  var loader = new BABYLON.AssetsManager(scene);
+  var load_task = loader.addMeshTask("habitat_v01", "", "assets/models/", "habitat_v01.obj");
+
+  load_task.onSuccess = function(task) {
+    console.log("SUCCESS")
+    console.log(task)
+    task.loadedMeshes[0].scaling.x = 0.015
+    task.loadedMeshes[0].scaling.y = 0.015
+    task.loadedMeshes[0].scaling.z = 0.015
+  }
+
+  load_task.onError = function(task, error) {
+    console.log("FAIL")
+    console.log(task)
+    console.log(error)
+  }
+
+  loader.load()
+
+  // BABYLON.SceneLoader.ImportMesh("batmanface", "batman.obj", scene, function (meshes) { 
+  // //   console.log("BLAH")
+  // });
 
   // Leave this function
   return scene;
@@ -105,16 +154,4 @@ engine.runRenderLoop(function () {
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
   engine.resize();
-});
-
-//When click event is raised
-window.addEventListener("click", function (evt) {
-  var pickResult = scene.pick(evt.clientX, evt.clientY);
-  var selected_tile = pickResult.pickedMesh;
-
-  var is_selected_material = new BABYLON.StandardMaterial("texture_xxx", scene);
-  is_selected_material.diffuseColor = new BABYLON.Color3(0, 1, 0);
-  is_selected_material.alpha = 0.35;
-
-  selected_tile.material = is_selected_material
 });
